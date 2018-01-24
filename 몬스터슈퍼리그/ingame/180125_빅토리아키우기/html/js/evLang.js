4f08ee05f6fdd3700e0e273f5c7c3091c564c5b0
김광희 <tony@433.co.kr>
2018-01-24 14:09:15 +0900

    evLang update

--- 몬스터슈퍼리그/ingame/180125_빅토리아키우기/html/js/evLang.js
+++ 몬스터슈퍼리그/ingame/180125_빅토리아키우기/html/js/evLang.js
@@ -35,9 +35,21 @@ function evLang (langType) {
         '★4<br>확정 스킬북 1개'
       ],
       btnName:
-      '<span class="btn_normal">보상받기</span>'+
-      '<span class="btn_end">받기완료</span>'
-    }, langEn = {     // 영어
+				'<span class="btn_normal">보상받기</span>'+
+				'<span class="btn_end">받기완료</span>',
+			successMessage: [
+				'마스터님, 모든 특명을 완료하는 날까지 힘내자구요!',
+				'우와, 에너지 받았다! 모험가서 저 레벨업시켜주실거죠?',
+				'좋은 보석이 나오면 꼭 저에게 주셔야 해요~? 약속★',
+				'에너지를 가지고 모험에 가면~♩ 레벨이 마구 오르죠~♪',
+				'신난다! 이 알을 소환하면 제게도 멋진 친구가 생기는 거에요?',
+				'마스터님, 저도 언젠가 3진화체가 될 수 있을까요?',
+				'골드에요, 골드! 우리 이제 부자다! 그쵸?',
+				"만약 알에서 또 다른 '빅토리아'가 나오면 저를 진화시켜주실건가요?",
+				'우리 드디어 모든 특명을 완수했어요★ 함께해서 너무 즐거웠어요!'
+			]
+    }, 
+		langEn = {     // 영어
       questTitle: [
         '10 Accumulated<br>Points',
         '50 Accumulated<br>Points',
@@ -61,8 +73,19 @@ function evLang (langType) {
         '4★<br>Skill Book X 1'
       ],
       btnName:
-      '<span class="btn_normal">Get Reward</span>'+
-      '<span class="btn_end">Completed</span>'
+				'<span class="btn_normal">Get Reward</span>'+
+				'<span class="btn_end">Completed</span>',
+			successMessage: [
+        'I will follow you till the end, Master!',
+        'Yes, It’s Energy! Time to level up!',
+        'Will you equip me a 6★ gem? Promise★',
+        'More Energies~♩ More Adventures~♪',
+        'Hooray! I hope they become my friends',
+        'Master, will I ever evolve into Nike?',
+        'It’s gold! And a lot of them too! We are rich now, right?',
+        'If there happens to be another Victoria, will you let me be the one to evolve?',
+        'It was my greatest pleasure traveling with you, Master!★'
+			]
     },
     langJa = {        // 일본어
       questTitle: [
@@ -88,9 +111,21 @@ function evLang (langType) {
         '★4<br>スキルブック 1個'
       ],
       btnName:
-      '<span class="btn_normal">受け取る</span>'+
-      '<span class="btn_end">受取済み</span>'
-    }, langHans = {       // 간체
+				'<span class="btn_normal">受け取る</span>'+
+				'<span class="btn_end">受取済み</span>',
+			successMessage: [
+        '主！特命を完遂するまで頑張りましょう！',
+        '主！エネルギーですよ！冒険ステージに連れてって、レベルアップしてください！',
+        'ジュエルは私にくださいね！約束★',
+        'エネルギーを使って冒険ステージに出ると～♩レベルが上がりますよ～♪ ',
+        'すっごーい！この卵がふ化すると私にも素敵なフレンズができますね！',
+        '主、いつになったら進化レベル3になれるでしょうか・・・？',
+        'ゴールドですよ、ゴールド！大金持ちですよ！ね？',
+        'もし卵から他の「ヴィクトリア」が出たら、進化してくれますよね・・・？',
+        '主！やっと特命を完遂しましたね★すっごく楽しかったです！'
+			]
+    }, 
+		langHans = {       // 간체
       questTitle: [
         '累积积分 <br>10个',
         '累积积分 <br>50个',
@@ -114,9 +149,21 @@ function evLang (langType) {
         '4★<br>技能书1本'
       ],
       btnName:
-      '<span class="btn_normal">领取奖励</span>'+
-      '<span class="btn_end">已领取</span>'
-    }, langHant = {         // 번체
+				'<span class="btn_normal">领取奖励</span>'+
+				'<span class="btn_end">已领取</span>',
+			successMessage: [
+        '星兽使者，一定要坚持到完成所有特别指令!',
+        '哇, 拿到能量啦! 快去开始冒险帮我升级吧~',
+        '如果出来好的宝石一定要镶嵌到我身上喔~ 拉钩★',
+        '拿着能量去冒险~♩ 让我们尽情升级吧~♪',
+        '开心! 没准这个蛋能召唤出帅气的星兽朋友喔~',
+        '星兽使者, 你看我什么时候可以完成3级进化形态呀?',
+        '金币！金币！我们现在是有钱人啦! 对吧?',
+        '如果又召唤出另一只 ‘维多利亚’的话，可帮我进化吗?',
+        '我们终于完成了所有的特别指令★ 很开心可以和你一起努力到最后!'
+			]
+    }, 
+		langHant = {         // 번체
       questTitle: [
         '累計點數 <br>10個',
         '累計點數 <br>50個',
@@ -139,25 +186,35 @@ function evLang (langType) {
         '(傳說)<br>神秘卵 1個',
         '4★<br>技能秘笈1本'
       ],
-      btnName:[
+      btnName:
         '<span class="btn_normal">領取獎勵</span>'+
-        '<span class="btn_end">已領取</span>'
+        '<span class="btn_end">已領取</span>',
+      successMessage: [
+        '星獸使者，一定要堅持完成所有特別指令!',
+        '哇, 拿到能量啦! 快去開始冒險幫我升級吧~',
+        '如果出來好的寶石一定要裝備到我身上哦~? 拉鉤★',
+        '拿著能量去冒險，讓我們盡情升級吧~♪',
+        '開心! 我猜這個卵可能會召喚出帥氣的星獸朋友~',
+        '星獸使者，你看我什麼時候可以完成3段進化形態呀?',
+        '金幣！金幣！我們現在是有錢人啦! 對吧?',
+        '如果能召喚出另一隻 ‘維多利亞’的話，可以幫我進化嗎?',
+        '我們終於完成了所有的特別指令★ 很開心可以和你一起努力到最後!'
       ]
     }
 
   if (langType == 'ko'){
-    return langKo
+    return langKo;
   }else if (langType == 'en'){
-    return langEn
+    return langEn;
   }else if(langType == 'ja'){
-    return langJa
+    return langJa;
   }else if(langType == 'th'){
-    return langTh
+    return langTh;
   }else if (langType == 'zh-CN'){
-    return langHans
+    return langHans;
   }else if (langType == 'zh-TW'){
-    return langHant
+    return langHant;
   }else{
-    return '존재하지 않는 언어입니다.'
+    return langEn;
   }
 }
