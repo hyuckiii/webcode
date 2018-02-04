d54a8bbddff869581602252bdf8e31624febd556
webcook <chefboy@naver.com>
2018-02-04 09:42:58 +0900

    핀크에서 귀신이 붙어왔나.. PC부터 시작해서.. 악몽 그자체.. 영문버전 국문복사해서 작업중

---
+++ 배틀붐/web/180206_사전예약/html/en/pc/js/web_ui.js
@@ -0,0 +1,32 @@
+/* UI */
+if(typeof jQuery !== 'undefined'){
+  $(document).ready(function(){
+
+    $('a[data-layer], button[data-layer], img.slick[data-layer]').modalLayer();
+    ezForm();
+
+    $('#videoMain').cover({
+      zoom:1.3                                  // 1.3미만은 고해상도에서 짤리는 문제 있음
+    });
+
+    var $carousel = $('.carousel');
+    $carousel.slick({
+      dots: true,
+      infinite: true,
+      mobileFirst: true
+    }); 
+  }); //document.ready
+}
+
+function ezForm(){
+  if ( $('#regForm').length > 0 ){
+    // input placeholder
+    $('#regForm .inp_mail input').focus(function(){
+      $(this).addClass('active');
+    }).blur(function(){
+      if($(this).val().length == 0){
+        $(this).removeClass('active');
+      }
+    });
+  }
+}
